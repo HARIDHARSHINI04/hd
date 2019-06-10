38d0306c607fa1c3043451345d550dc09962fa2f
HARIDHARSHINI04 <51364162+HARIDHARSHINI04@users.noreply.github.com>
2019-06-10 10:35:29 +0530

    Update vc.py

--- vc.py
+++ vc.py
@@ -1,7 +1,7 @@
 n=input()
 if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u' or n=='A' or n=='E' or n=='I' or n=='O' n=='U'):
    print("vowel")
-elif n in(bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ):
+elif n in('bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'):
    print("consonant")
 else:
    print("invalid")
