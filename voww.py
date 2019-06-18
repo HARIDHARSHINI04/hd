3c2d0461b0335a877ac3c7e0f27cd8c5666c6daa
HARIDHARSHINI04 <51364162+HARIDHARSHINI04@users.noreply.github.com>
2019-06-18 10:34:00 +0530

    Update voww.py

--- voww.py
+++ voww.py
@@ -2,6 +2,6 @@ g=input()
 for i in range(0,len(g)):
   if(g[i]=='a' or g[i]=='e' or g[i]=='i' or g[i]=='o' or g[i]=='u' or g[i]=='A' or g[i]=='E' g[i]=='I' or g[i]=='O' or g[i]=='U'):
          print("yes")
-         break
+         break;
 else:
          print("no")
